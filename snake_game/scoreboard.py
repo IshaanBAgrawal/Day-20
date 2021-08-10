b35bcb1e163c917b2a0afefb5ab7a8b2636ae72e
Ishaan <agrawalishaan115@gmail.com>
2021-08-10 15:12:47 +0530

    Snake game (Day 20 and 21 work)

---
+++ snake_game/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.score = 0
+        self.color("white")
+        self.goto(x=0, y=270)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"Score: {self.score}", align="center", font=("Courier", 15, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
+
+    def game_over(self):
+        self.goto(x=0, y=0)
+        self.write("GAME OVER", align="center", font=("Courier", 25, "normal"))
